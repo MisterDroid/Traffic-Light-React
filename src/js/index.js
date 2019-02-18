0af495dbadd7f8a28d5439c6b8a1659d5644a4b4
Josue Rodriguez <rodzcruz.josue@gmail.com>
2019-02-18 20:29:41 +0000

    First commit. Completed Traffic light using React

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+import TrafficLight from "./component/TrafficLight.jsx";
+
+//import your own components
+
+//render your react application
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
